68d022bd90c93f6e1b7679f1e83041627e3cb0ee
jimmy-hawtsauce <farbodnajafi@outlook.com>
2019-08-22 16:24:07 +0430

    Required Login for profile page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
@@ -17,5 +17,6 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
