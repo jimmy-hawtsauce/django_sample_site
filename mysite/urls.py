276629536726d8c081a9854d896000f781bdec5d
jimmy-hawtsauce <farbodnajafi@outlook.com>
2019-09-03 20:28:43 +0430

    'Password reset done' route defined.

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,6 +31,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
          name='password-reset'),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password-reset-done'),
 ]
 
 if settings.DEBUG:
